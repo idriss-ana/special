b71886aa71aa48e07eac30fede43a9e995a42a4e
idriss-ana <110461059+idriss-ana@users.noreply.github.com>
2023-05-19 22:43:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,16 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "Happy Birthdayyyyyyy",     
+        "Happy Birthday",     
         "19/05/2003",  
         "19/05/2023",
         "20 Years",
-        "On your special day, I want you to know how much you mean to me. ",
-        "Another year older,but you're still as amazing as ever",
-        "Remember to take care of yourself ",
-        "keep chasing your dreams and living your best life",
-        "Happy birthdayyyyyy again to you ",
+        "On your special day, I want you to know how much you mean to me. Thank you for being an amazing kind soul. Happy birthday!",
+        "Thank you for always being there for me. Here's to another year of adventures!",
+        "Another year older, but you're still as amazing as ever",
+        "You're important and you matters,Remember to take care of yourself, both physically and mentally. ",
+        "keep chasing your dreams and living your best life. I can't wait to see where life takes you next, and to catch up when we  Take care till then!",
+        "Happy birthday again to my favorite companion in adventure, laughter, and all things fun, weird and wonderful",
         "Nesrine Abdelhak .. ",
     ],
 
@@ -25,20 +26,10 @@ var config = {
 
 
 
-    /**
-     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
-     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
-     * 例如
-     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
-     *
-     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
-     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
-     */
+
     imgs: {
         "心爱的小可爱": "./imgs/IMG_20230407_005828_600.jpg",
-        // "今天是你的生日": "./imgs/birthday.jpg",
     },
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "can you turn on the light?",
         play: "press me",
